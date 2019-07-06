d9f13c2e5d7b90151df5bb87cad587824684f6b8
Martin Terskin <martinas.terskin@gmail.com>
2019-07-06 16:37:13 -0400

    initial commit

---
+++ config-overrides.js
@@ -0,0 +1,37 @@
+// /* config-overrides.js */
+
+const {
+    override,
+    addDecoratorsLegacy,
+    disableEsLint,
+    addBundleVisualizer,
+    addWebpackAlias,
+    adjustWorkbox,
+    addBabelPlugins,
+    fixBabelImports,
+    babelInclude,
+    removeModuleScopePlugin
+  } = require("customize-cra");
+
+  const path = require("path");
+  
+module.exports = override(
+    disableEsLint(),
+    removeModuleScopePlugin(),
+    ...addBabelPlugins(
+      "polished",
+      "emotion",
+      "babel-plugin-transform-do-expressions"
+    ),
+    fixBabelImports("lodash", {
+      libraryDirectory: "",
+      camel2DashComponentName: false
+    }),
+    fixBabelImports("react-feather", {
+      libraryName: "react-feather",
+      libraryDirectory: "dist/icons"
+    }),
+    babelInclude([
+        path.resolve("src"), // make sure you link your own source
+      ])
+  );
